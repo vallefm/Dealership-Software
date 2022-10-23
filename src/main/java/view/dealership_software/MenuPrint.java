9255ccd7277da518ca30fb08b74c131e5671d80c
Ethan_Willingham <568352@gmail.com>
2022-10-23 11:35:13 -0500

    N/A

--- src/main/java/view/dealership_software/MenuPrint.java
+++ src/main/java/view/dealership_software/MenuPrint.java
@@ -6,8 +6,8 @@ public class MenuPrint {
         String message = """
                 ReadJSON - Read JSON file from your C: drive(File chooser may appear behind this window)\s  CHECK
                 AddCar - Add a car to a dealership\s                                    CHECK
-                DealerOff - Turn off the dealership acquisition\s
-                DealerOn - Turn on the dealership acquisition\s
+                DealerOff - Turn off the dealership acquisition\s                       CHECK
+                DealerOn - Turn on the dealership acquisition\s                   CHECK 
                 ShowList - Show list of all current vehicles from a dealership\s
                 ExportFromDealer - Export all vehicles from a dealership into a single JSON file\s
                 Create - Create a dealership\s
