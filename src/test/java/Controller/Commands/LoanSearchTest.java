0c94aad0ede577656a7bd144dc2e823494959f7e
XuezengYang15 <112597516+XuezengYang15@users.noreply.github.com>
2022-10-25 17:42:14 -0500

    LoanSearchTest is done.

--- src/test/java/Controller/Commands/LoanSearchTest.java
+++ src/test/java/Controller/Commands/LoanSearchTest.java
@@ -1,5 +1,8 @@
 package Controller.Commands;
 
+import Models.Company;
+import Models.Dealer;
+import Models.Vehicle;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,5 +10,31 @@ import static org.junit.jupiter.api.Assertions.*;
 class LoanSearchTest {
 
     @Test
-    void loanTest() {}
+    void loanSearch() {
+
+        String dealershipId = "111";
+        String vehicleType = "SUV";
+        String vehicleManufacturer = "Toyota";
+        String vehicleModel = "4Runner";
+        String vehicleId = "123f";
+        int vehiclePrice = 50000;
+        long acquisition_date = 1111111;
+        Boolean isLoan = false;
+
+        Vehicle newCar = new Vehicle(dealershipId, vehicleType, vehicleManufacturer, vehicleModel, vehicleId, vehiclePrice, acquisition_date);
+
+        Company.getCompany().add(new Dealer(dealershipId, true));
+
+        Dealer dealer = new Dealer();
+        LoanSearch ls= new LoanSearch();
+
+        //Add car with the above attributes to list of cars at dealership
+        dealer.getListOfCarsAtDealer().add(newCar);
+
+        /*
+        The method loanSearch returns a boolean array with 2 indexes. The first index is the invalid_CarId boolean,
+        and the second index is the carLoanedStatus boolean.
+         */
+        assertArrayEquals(new boolean[]{true, false}, ls.loanSearch(vehicleId) );
+    }
 }
